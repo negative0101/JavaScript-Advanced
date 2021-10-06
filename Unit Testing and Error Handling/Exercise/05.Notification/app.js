d56b583389bf995afb9c6ef3d4c37bcf4e6c0e36
Ivaylo Ivanov <64315685+negative0101@users.noreply.github.com>
2021-10-06 21:13:40 +0300

    Add files via upload

---
+++ Unit Testing and Error Handling/Exercise/05.Notification/app.js
@@ -0,0 +1,7 @@
+function notify(message) {
+  const resultDiv = document.querySelector('#notification');
+  resultDiv.style.display = 'block';
+  resultDiv.textContent = message
+  resultDiv.addEventListener('click', () => { resultDiv.style.display = 'none' })
+
+}
