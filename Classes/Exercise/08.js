08e135213bc8bbbfa8d1a09f42aae66a0e65639c
Ivaylo Ivanov <64315685+negative0101@users.noreply.github.com>
2021-10-09 22:01:18 +0300

    Add files via upload

---
+++ Classes/Exercise/08.js
@@ -0,0 +1,34 @@
+function solve(arr) {
+    const juices = {}
+    const bottles = {}
+
+    const addBottles = (n, q) => {
+        const btlToAdd = (q - (q % 1000)) / 1000
+
+        if (btlToAdd > 0) {
+            bottles[n] = (bottles[n] || 0) + btlToAdd
+            return q % 1000
+        }
+
+        return q
+    }
+
+    arr.forEach(x => {
+        const [name, quantity] = x.split(' => ')
+        juices[name] = juices[name] || 0
+
+        juices[name] = addBottles(name, juices[name] + Number(quantity))
+
+    })
+
+    return Object.entries(bottles)
+        .map(([name, quantity]) => `${name} => ${quantity}`)
+        .join('\n')
+}
+
+console.log(solve(['Orange => 2000',
+    'Peach => 1432',
+    'Banana => 450',
+    'Peach => 600',
+    'Strawberry => 549'])
+)
